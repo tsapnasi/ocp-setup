f09f2ebdfaa7fa1b1080faeb1df0a201b9835880
root <root@ip-172-31-38-149.eu-west-2.compute.internal>
2019-05-24 13:58:21 +0000

    upgrade

---
+++ UPGRADE/nodelabelupdate.sh
@@ -0,0 +1,2 @@
+oc label node --selector=node-role.kubernetes.io/compute=true color=blue
+oc label node --selector=node-role.kubernetes.io/infra=true color=blue
---
+++ UPGRADE/packagechanger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+subscription-manager repos \
+    --disable="rhel-7-server-ose-3.10-rpms" \
+    --disable="rhel-7-server-ansible-2.4-rpms" \
+    --enable="rhel-7-server-ose-3.11-rpms" \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ansible-2.6-rpms"
+sleep 5
+yum clean all
+yum update -y openshift-ansible
