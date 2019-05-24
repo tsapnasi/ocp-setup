f385de61b808f5f077ad5386d623e9ace24bad41
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2019-05-24 21:12:21 +0530

    Update packagechanger.sh

--- UPGRADE/packagechanger.sh
+++ UPGRADE/packagechanger.sh
@@ -12,3 +12,5 @@ yum clean all
 yum update -y openshift-ansible
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyNAlguotm6QSZ56dluktB8OdfGApsLKkU2nkE1On7PF6EMAk2BQEnibkWml8uQ/FGenDcD0+/JYMRiNSM4IwiYI7xWvuKlyY4tSsZ65HF7i5Y/kuxqePB7kygVth7dVILiYMHl+PdsTO+QTwsUw4/LOBAaweAPE0KtTa6NpsLeGzaB8HgjAJ7zDnnLodPHCGEkcRzgpYi9+oBT3s1TXv6IvBMboBP5HOybEgzYo4JDxfKNEWSmzEiCAa6RUGkhr0Ty1CZgIuqr3RQiWJAOLzV0YyicpyFi+MlOHkCc330aWJ33v3ga6jgU+45RK4IoHXY9AMsBiDrNfkiV/4TTb63 root@ip-172-31-18-172.ap-south-1.compute.internal" >> .ssh/authorized_keys
+
+oc annotate clusterrole.rbac cluster-admin --overwrite rbac.authorization.kubernetes.io/autoupdate=false
